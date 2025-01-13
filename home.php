053a701a503b8c32652de6b2c056363d33807a22
mariam <mariamemampp00@gmail.com>
2025-01-13 15:01:52 +0200

    REQUEST is bring the data from request and even if we use get method or post method

--- home.php
+++ home.php
@@ -2,5 +2,14 @@
 echo "<pre>";
 var_dump($_POST);
 echo "</pre>";
+echo "********************";
+echo "<pre>";
+var_dump($_GET);
+echo "</pre>";
+echo "********************";
+
+echo "<pre>";
+var_dump($_REQUEST);
+echo "</pre>";
 
 ?>
