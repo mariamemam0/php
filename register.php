6f8b58cda3ae29cce99a6ff4e7c0780e1030bf56
mariam <mariamemampp00@gmail.com>
2025-01-13 14:51:10 +0200

    data is appear in the url if we use the method get

---
+++ register.php
@@ -0,0 +1,15 @@
+<html>
+<form action="home.php"method="get">
+    First Name<input type="text" placeholder="First Name" name="fname" ><br>
+
+    Last Name<input type="text" placeholder="Last Name" name="lname" ><br>
+
+    Email<input type="text" placeholder="Email" name="email"><br>
+
+    Address<input type="text" placeholder="Address" name="addr"><br>
+
+    phone<input type="text" placeholder="phone" name="phone" ><br>
+
+    <input type="submit" value="Register">
+</form>
+</html>
