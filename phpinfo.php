921a2dc243e471df4b5373a0bbfc82f06913c4c8
mariam <mariamemampp00@gmail.com>
2025-01-13 12:21:17 +0200

    info

--- phpinfo.php
+++ phpinfo.php
@@ -1,11 +1,11 @@
 <?php
 
-$_COOKIE
-$_SESSION
+$_COOKIE //used in authentiendication and save the data in the browser 
+$_SESSION//used in authentiendication and save the data in the browser 
 $_ENV
-$_FILES
-$_GET
-$_POST
-$_REQUEST
-$_SERVER
+$_FILES //used in file upload to get the file information
+$_GET //used to get the data from the request
+$_POST //used to get the data from the request
+$_REQUEST //used to get the data from the request
+$_SERVER //used to get the server information
 ?>
