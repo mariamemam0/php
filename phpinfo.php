a951517cf119ba046fa7706a723e79c09cbb78e4
mariam <mariamemampp00@gmail.com>
2025-01-13 12:23:22 +0200

    var_dump to print array or variable

--- phpinfo.php
+++ phpinfo.php
@@ -1,11 +1,7 @@
 <?php
 
-$_COOKIE //used in authentiendication and save the data in the browser 
-$_SESSION//used in authentiendication and save the data in the browser 
-$_ENV
-$_FILES //used in file upload to get the file information
-$_GET //used to get the data from the request
-$_POST //used to get the data from the request
-$_REQUEST //used to get the data from the request
-$_SERVER //used to get the server information
+
+$_SERVER ;//used to get the server information
+var_dump($_SERVER);
+
 ?>
