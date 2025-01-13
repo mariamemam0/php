17e394faf74e0a0a5b01ecaa55dc584c2ff1930d
mariam <mariamemampp00@gmail.com>
2025-01-13 13:03:36 +0200

    try continue

--- phpinfo.php
+++ phpinfo.php
@@ -1,5 +1,14 @@
 <?php
 
-isset() // checks if a variable has been set
+$array = [1,2,3,4,5,6,7];
+echo "<ul>";
+for($i = 0; $i<count($array); $i++){
+    if($array[$i] == 5){
+        continue;
+    }
+     echo  "<li>".$array[$i]."</li>";
 
+}
+echo "</ul>";
+echo "*****After for******";
 ?>
