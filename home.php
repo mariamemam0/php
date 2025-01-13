0db1cbbc1d7e20025595dd2efe7a658b1f2a7231
mariam <mariamemampp00@gmail.com>
2025-01-13 16:09:38 +0200

    make table to put data from register file

--- home.php
+++ home.php
@@ -1,3 +1,12 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
 <?php
 echo "<ul>";
 echo "<li>{$_POST['fname']}</li>";
@@ -8,23 +17,25 @@ echo "<li>{$_POST['phone']}</li>";
 echo "</ul>";
 
 ?>
-<table>
-<tr>
-    <th>First Name</th>
-    <th>Last Name</th>
-    <th>Email</th>
-    <th>Address</th>
-    <th>Phone</th>
+<table class="table table-dark">
+<tr class="table-active">
+    <th scope="row">First Name</th>
+    <th scope="row">Last Name</th>
+    <th scope="row">Email</th>
+    <th scope="row">Address</th>
+    <th scope="row">Phone</th>
 </tr>
-<tr>
+<tr class="table-active">
 <?php 
 foreach($_POST as  $value){
-    echo "<td>$value</td>";
+    echo "<td class='table-active'>$value</td>";
 }
 ?>
     </tr>
 
 </table>
+</body>
+</html>
 
 
 
