c6ab0e3f0ed1ff793cc399be7d20f4f0e81d10ff
mariam <mariamemampp00@gmail.com>
2025-01-13 15:24:46 +0200

    way to get data from request to home page

--- home.php
+++ home.php
@@ -1,15 +1,30 @@
 <?php
-echo "<pre>";
-var_dump($_POST);
-echo "</pre>";
-echo "********************";
-echo "<pre>";
-var_dump($_GET);
-echo "</pre>";
-echo "********************";
-
-echo "<pre>";
-var_dump($_REQUEST);
-echo "</pre>";
+echo "<ul>";
+echo "<li>{$_POST['fname']}</li>";
+echo "<li>{$_POST['lname']}</li>";
+echo "<li>{$_POST['email']}</li>";
+echo "<li>{$_POST['addr']}</li>";
+echo "<li>{$_POST['phone']}</li>";
+echo "</ul>";
 
 ?>
+<table>
+<tr>
+    <th>First Name</th>
+    <th>Last Name</th>
+    <th>Email</th>
+    <th>Address</th>
+    <th>Phone</th>
+</tr>
+<tr>
+    <td><?= $_POST['fname']?></td>
+    <td><?= $_POST['lname']?></td>
+    <td><?= $_POST['email']?></td>
+    <td><?= $_POST['addr']?></td>
+    <td><?= $_POST['phone']?></td>
+
+</table>
+
+
+
+
