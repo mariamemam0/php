d01aefbd10a89c5f7aeba959f454aaf89a68a6e1
mariam <mariamemampp00@gmail.com>
2025-01-14 15:16:04 +0200

    use rewind to put the qursor at the biginng

--- file.php
+++ file.php
@@ -7,18 +7,20 @@
 
  //readfile("data.txt");
 
- $data = file("data.txt");
-/*
+ //$data = file("data.txt");
+
 
 //open file 
- $file = fopen("data.txt","w");
+ $file = fopen("data.txt","w+");
 
 //write file
 fwrite($file ,"mariam emam ,mariamemem@com,01552"."\n");
 fwrite($file ,"maram emam ,mariamemem@com,0155"."\n");
 fwrite($file ,"farida emam ,mariamemem@com,0155");
-
-
+rewind($file);
+while(!feof($file)){
+   echo fgets(stream: $file)."<br>";
+}
 
 //close file
 fclose($file);
@@ -26,10 +28,7 @@ fclose($file);
 
 /////////////
 //to Read the data that i save
-$file = fopen("data.txt","r");
-while(!feof($file)){
-   echo fgets(stream: $file)."<br>";
-}
-*/
+
+
 
 ?>
