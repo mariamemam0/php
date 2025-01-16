4b54aad202bc148bd55a1b4f7fe634857a8ba5e8
mariam <mariamemampp00@gmail.com>
2025-01-16 15:23:57 +0200

    function range to give range of numbers

--- array.php
+++ array.php
@@ -4,6 +4,14 @@ $array = [2,4,5,6,7];
 $arraydata= array(1,2,3,4,5,6);
 
 
+
+$data = range(1,200);
+for($i=0;$i<count($data);$i++){
+    echo $data[$i]."<br>";
+}
+
+
+
 $array_assoc=[
     "id"=>1,
     "name"=>"mariam",
@@ -11,3 +19,4 @@ $array_assoc=[
     "data",
     "data2"
 ];
+
