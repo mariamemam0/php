56e340f737a25c5587f07f4d344187489a9b5529
mariam <mariamemampp00@gmail.com>
2025-02-03 08:19:11 +0200

    make add.php to save data from registration , save it into txt file

---
+++ add.php
@@ -0,0 +1,11 @@
+<?php 
+//file to write inside it
+//1-fopen - fwrite- fclose
+//2-file_put_content(file,data,file_append)
+
+
+ $data = implode(",",$_GET);
+file_put_contents("data.txt","\n".$data,FILE_APPEND);
+
+
+?>
